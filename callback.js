33ab56d4a983d5c7fdf767a77f7f2a2e56a2718c
Sunay Gökçe İnce <isunaygokce@gmail.com>
2022-09-28 16:10:51 +0300

    fetch ile users ve post terminale yazdırıldı.

--- callback.js
+++ callback.js
@@ -1,4 +1,11 @@
 import fetch from "node-fetch";
 
 fetch("https://jsonplaceholder.typicode.com/users")
-  .then(data => console.log(data));
+  .then((data) => data.json())
+  .then(users => {
+     console.log("users yuklendi", users)
+
+      fetch("https://jsonplaceholder.typicode.com/posts/1")
+        .then((data) => data.json())
+        .then(post => console.log("post yuklendi", post))
+  });
