4356cf1981ed5e705be964c5d38a22e6ca115092
sunay gokce ince <sgokceince@icloud.com>
2022-10-05 13:44:49 +0300

    some değişkeni some array fonksiyonu içinde yazıldı ve arrayin verdiğimiz koşula uyan elemanlardan birine uyup uymadığı bakıldı uyuyorsa true döner uymuyorsa false döner

--- array-function.js
+++ array-function.js
@@ -5,11 +5,11 @@ const users = ["Sakiz", "Gusta", "Sunay"];
 const users = [
     {
         name: "Sakiz",
-        age: "3"
+        age: 3
     },
     {
         name: "Gusta",
-        age: "5"
+        age: 5
     },
     {
         name: "Sunay"
@@ -52,6 +52,14 @@ const result = users.find((item) => item.name === "Sakiz" && item.age > 2);
 */
 
 //filter filtreleme yapar.
-
+/*
 const filtered = users.filter((item) => item.name ==="Gusta" && item.age <= 5)
   console.log(filtered)
+
+*/
+
+//some arrayin içindeki elemanlardan birnin verdiğimiz koşula uyup uymadıgına bakar uyuyorsa true döner uymuyorsa false döner
+   
+ // const some = users.some(item => item.age === 10); false
+    const some = users.some((item) => item.age === 3 )
+   console.log(some)
